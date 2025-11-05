c49655f7214a32008d96dcd234b3d7c01774be9d
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-11-05 20:44:41 +0300

    feat: Add LightRays component and update authentication forms with new UI elements

--- kaslan/src/pages/auth/Register.tsx
+++ kaslan/src/pages/auth/Register.tsx
@@ -1,18 +1,25 @@
-import Silk from '@/components/Silk'
+import LightRays from '@/components/LightRays'
+import { SignupForm } from '@/components/signup-form'
 
 function Register() {
     return (
-        <div className='w-full h-full'>
-            <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
-
-
-                <Silk
-                    speed={5}
-                    scale={1}
-                    color="#5227ff"
-                    noiseIntensity={1.5}
-                    rotation={0}
+        <div className='w-full h-full bg-black'>
+            <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+                <LightRays
+                    raysOrigin="top-center"
+                    raysColor="#ffffff"
+                    raysSpeed={1.5}
+                    lightSpread={0.8}
+                    rayLength={1.2}
+                    followMouse={true}
+                    mouseInfluence={0.1}
+                    noiseAmount={0.1}
+                    distortion={0.05}
+                    className="custom-rays"
                 />
+                <SignupForm className='absolute top-18 left-1/4' style={{
+                    width: "50vw"
+                }} />
             </div>
         </div>
     )
