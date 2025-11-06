a77991291cf5b210742bc708bf69c38399f959e0
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-11-07 00:58:31 +0300

    feat: add navigation link and update button text in ForYou component

--- kaslan/src/pages/home/components/for-you.tsx
+++ kaslan/src/pages/home/components/for-you.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
+import { Link } from "react-router-dom"
 
 function ForYou() {
     return (
@@ -18,11 +19,39 @@ function ForYou() {
                         </span>
                     </div>
                 </div>
-                <Button variant={"ghost"}>
-                    Takip Et
+                <Button className="text-xs font-semibold text-accent-foreground" variant={"ghost"}>
+                    Geçiş Yap
                 </Button>
             </div>
             <Separator />
+            <div className="flex flex-row w-full gap-4">
+                <span className="text-md text-muted-foreground font-semibold">Senin için önerilen</span>
+                <Link to="/all" className="ml-auto text-xs font-semibold text-accent-foreground hover:underline items-center justify-center flex">
+                    Tümünü Gör
+                </Link>
+            </div>
+            <div className="flex flex-col gap-2 w-full">
+                {[1, 2, 3, 4, 5].map(() => (
+                    <div className="flex w-full flex-row flex-wrap justify-between items-center gap-2 ">
+                        <div className="flex flex-row items-center justify-between gap-2">
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <div className="flex flex-col">
+                                <span className="font-medium">shadcn</span>
+                                <span className="text-xs text-muted-foreground">
+                                    kubra_kilicaslan + 34 diğer kişi tak...
+                                </span>
+                            </div>
+                        </div>
+                        <Button className="text-xs font-semibold text-accent-foreground" variant={"ghost"}>
+                            Takip Et
+                        </Button>
+                    </div>
+                ))}
+
+            </div>
         </div>
     )
 }
